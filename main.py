59c6f3a4f86dad6704469fdd19c800f5a6eca102
Chris Atkinson <46910324+chrisatk@users.noreply.github.com>
2022-10-28 14:43:26 +0000

    Added comments

--- main.py
+++ main.py
@@ -14,9 +14,13 @@ options = [
   "retake"
 ]
 
+# Loop through each of the options
 for option in options:
-  check = ""
+  check = "" # initialize an empty string to check against the answer
+  # Loop through each character in the word
   for char in option:
+    # Convert the characters to the binary values in the key dict
     check = check + key[char]
   if check == answer:
+    # Compare the converted option to the answer to find a winner
      print(option+" is the answer")
